31ebbb1d56220acf5788e8ac931327119fa99dc5
z4yt0s <zaytos_the_catcher@protonmail.com>
2024-03-21 11:27:48 +0100

    implements new form to execute program (StartCSP)

--- src/csp.py
+++ src/csp.py
@@ -9,34 +9,20 @@ from sys import exit
 
 from modules.CreateSecurePassword import CreateSecurePasswords
 from modules.Visuals import Visuals
-from utils.arguments import start_args, check_and_set_args
-
-def ctrl_c(sig, frame, vs: Visuals=Visuals()) -> NoReturn:
-    """
-    Handle Ctrl + C interruption.
-    Args:
-        sig: The signal number.
-        frame: The current stack frame
-        vs (Visuals): Instance of Visuals for pretty output
-    Return: 
-        NoReturn (exit)
-    """
-    vs.console.print(f'\n[!] Exiting. . .', style='bold red3')
-    exit(0)
+from modules.StartModes import StartCSP
+from utils.arguments import start_args
 
 def main() -> NoReturn:
-    signal(SIGINT, ctrl_c)
-
-    vs: Visuals = Visuals()
-    vs.banner()
-
     args: Namespace = start_args()
-    password = check_and_set_args(args, vs)
+    start_csp: StartCSP = StartCSP()
+    
+    mode = start_csp.detect_mode(args)
+    mode.start_mode()
 
-    csp: CreateSecurePasswords = CreateSecurePasswords(password, args.separator)
-    force_password = csp.create_strong_pass()
-
-    vs.console.print(f'[bold green4][+] Final Password: {force_password}[/bold green4]')
+    #password = check_and_set_args(args, vs)
+    #csp: CreateSecurePasswords = CreateSecurePasswords(password, args.separator)
+    #force_password = csp.create_strong_pass()
+    #vs.console.print(f'[bold green4][+] Final Password: {force_password}[/bold green4]')
 
 if __name__ == '__main__':
     main()
