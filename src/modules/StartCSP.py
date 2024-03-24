04cd7192a071aef12487c1c8c2696fc8d5b5f8a7
z4yt0s <zaytos_the_catcher@protonmail.com>
2024-03-24 19:51:01 +0100

    capture ^C in creation of masterkey (First execute), adding more information and text to create masterkey, check if masterkey its a strong password, controlL and controlC handle

--- src/modules/StartCSP.py
+++ src/modules/StartCSP.py
@@ -6,7 +6,6 @@ from typing import (
     Dict,
     List,
     NoReturn, 
-    Callable
 )
 from functools import wraps
 from argparse import Namespace
@@ -18,10 +17,12 @@ from prompt_toolkit.completion import NestedCompleter as NestComp
 from prompt_toolkit.shortcuts import CompleteStyle
 from prompt_toolkit.cursor_shapes import CursorShape
 from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.keys import Keys
 
 # own libraries
 from modules.DataManagement import DataManagement
 from modules.Visuals import Visuals
+from modules.CreateSecurePassword import CreateSecurePasswords # change
 
 class StartCSP:
     """
@@ -33,15 +34,12 @@ class StartCSP:
         AUTH_QUESTION (ClassVar[List[Tuple[str, str]]]): Representing the 
         autentication question presented to the user, with the format for
         required to 'message' from PromptSession class.
-        PROMPT_STYLE (ClassVar[Style]): Representing the style of the 
-        prompt displayed to the user.
         _authenticated (bool): a flag indicating whether the user has been
         successfull authenticated.
     """
     AUTH_QUESTION: ClassVar[List[Tuple[str, str]]] = [
         ('class:msg', '[^] Enter the masterkey: ')
     ]
-    PROMPT_STYLE: ClassVar[Style] = Style.from_dict({'msg': '#227547 bold'})
     _authenticated = False
 
     def __init__(self) -> None:
@@ -51,11 +49,26 @@ class StartCSP:
             self.vs.banner()
             self.tmp_session: PromptSession = PromptSession(
                 message=StartCSP.AUTH_QUESTION,
-                style=StartCSP.PROMPT_STYLE,
-                is_password=True
+                style=Style.from_dict({'msg': '#00b44e'}),
+                is_password=True,
+                key_bindings=self._tmp_kb()
             )
             self.check_masterkey()
     
+    def _tmp_kb(self):
+        tmp_kb = KeyBindings()
+
+        @tmp_kb.add(Keys.ControlC)
+        #def stop_current_exec(event):
+        def stop_current_exec(event):
+            raise KeyboardInterrupt
+        
+        @tmp_kb.add(Keys.ControlL)
+        def stop_current_exec(event):
+            raise KeyboardInterrupt
+    
+        return tmp_kb
+
     def check_masterkey(self) -> NoReturn:
         """
         Check if the masterkey exits and realize the user authentication for
@@ -74,21 +87,42 @@ class StartCSP:
         user to provide a master key for the first time the CSP is run. The
         user will be asked to type the master key twice to verify it.
         """
-        first_text: str = '''
-            [*] The first time CSP is run, you must provide a master
-            key, then you will have to type the master key twice to
-            verify it.
-        '''
-        self.vs.print(first_text, style='inf')
+        msg0: str = 'The first time cps is run, a master key must be created'
+        msg1_0: str = 'The master key will be used to encrypt passwords and as'
+        msg1_1: str = 'an authentication method'
+        msg2: str = 'The master key must have the following requirements'
+        msg2_content: List[str]= [
+            'Minimum length of eight characters',
+            'At least one upper and one lower case letter',
+            'Contain at least one number',
+            'At least one special character',
+        ]
+        self.vs.print(msg0, type='inf')
+        self.vs.print(f'{msg1_0} {msg1_1}', type='inf', end='\n')
+        self.vs.print(msg2, type='war')
+        self.vs.print(msg2_content, type='list')
         while True:
-            masterkey0: str = self.tmp_session.prompt()
-            if masterkey0 != self.tmp_session.prompt():
-                self.vs.print('Password must be the same', type='err')
-                continue
-            break
+            try:
+                masterkey0: str = self.tmp_session.prompt()
+                if masterkey0 != self.tmp_session.prompt():
+                    self.vs.print('Password must be the same', type='err')
+                    print()
+                    continue
+                if not CreateSecurePasswords.is_strong_password(masterkey0):
+                    self.vs.print(
+                        'The password does not meet the requirements',
+                        type='err'
+                    )
+                    print()
+                    continue
+                break
+            except KeyboardInterrupt:
+                self._exit_csp()
+            except EOFError:
+                self._exit_csp()
         query: str = self.data_mgmt.predefined_sql('set_masterkey')
-        self.data_mgmt.cursor.execute(query)
-        self.vs.print('Masterkey inserted correctly', type='info')
+        self.data_mgmt.cursor.execute(query, (masterkey0,))
+        self.vs.print('Masterkey inserted correctly', type='inf', end='\n')
     
     def _check_credentials(self):
         """
@@ -286,7 +320,7 @@ class StartCSP:
         """
         self.vs.print('Save and closing connection to database', type='inf')
         self.data_mgmt.save_and_exit(True)
-        self.vs.print('Exiting...', type='err')
+        self.vs.print('Exiting..', type='err')
         exit(0)
 
 class PromptCSP(StartCSP):
@@ -321,6 +355,8 @@ class PromptCSP(StartCSP):
     def __init__(self) -> None:
         super().__init__()
         self.csp_session: PromptSession = PromptSession(
+            message=[('class:prompt', '\nCSP> ')],
+            style=Style.from_dict({'prompt': '#227547 bold'}),
             completer=NestComp.from_nested_dict(PromptCSP.CSP_COMPLETER),
             complete_style=CompleteStyle.READLINE_LIKE,
             cursor=CursorShape.BEAM,
@@ -331,11 +367,14 @@ class PromptCSP(StartCSP):
     def _create_kb(self) -> KeyBindings:
         kb = KeyBindings()
 
-        @kb.add('<sigint>')
+        @kb.add(Keys.ControlC)
         def stop_current_exec(event):
-            raise KeyboardInterrupt
+            # insert ^C in buffer
+            event.app.current_buffer.insert_text('^C')
+            # execute Enter
+            event.app.current_buffer.validate_and_handle()
 
-        @kb.add('c-d')
+        @kb.add(Keys.ControlD)
         def exit_csp_kb(event):
             # insert exit in buffer
             event.app.current_buffer.insert_text('exit')
@@ -350,8 +389,8 @@ class PromptCSP(StartCSP):
                 instruction: str = self.csp_session.prompt('\nCSP> ')
                 self._check_command(instruction)
             except KeyboardInterrupt:
-                continue
-    
+                self._check_command('^C')
+
     def _proc_instruction(self, instruction: str) -> Tuple[str, List[str]]:
         command, *args = instruction.strip().split(' ')
         args = list(filter(lambda arg: arg != '', args))
@@ -372,6 +411,8 @@ class PromptCSP(StartCSP):
                 self._help(args)
             case 'exit':
                 self._exit_csp()
+            case '^C':
+                pass
             case _:
                 self.vs.print('Command Not Fount: try [CSP> help]', type='err')
 
